6839e2fbb2995dd03b5d94a64e9bf170797f40f2
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-05-28 03:05:25 +0700

    core.gather -> implemented (fn)getMapContent&(fn)setMapContent

---
+++ src/core/gather/index.ts
@@ -0,0 +1,36 @@
+import { IConfig } from "../../interfaces/common.interfaces";
+import fetch, { Response } from "node-fetch";
+import { URLSearchParams } from "url";
+import { GATHER_GET_MAP_URL, GATHER_SET_MAP_URL } from "../../constants/vars";
+export async function getMapContent(
+  gatherConf: IConfig["gatherCredential"]
+): Promise<any> {
+  const params: URLSearchParams = new URLSearchParams();
+  params.append("apiKey", gatherConf.apiKey);
+  params.append("spaceId", gatherConf.spaceId);
+  params.append("mapId", gatherConf.mapId);
+  const response: Response = await fetch(
+    `${GATHER_GET_MAP_URL}?` + params.toString(),
+    {
+      method: "GET",
+    }
+  );
+  const jsonResponse = await response.json();
+  return jsonResponse;
+}
+
+export async function setMapContent(
+  gatherConf: IConfig["gatherCredential"],
+  mapContent: any
+): Promise<any> {
+  const response: Response = await fetch(GATHER_SET_MAP_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      ...gatherConf,
+      mapContent,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+  const jsonResponse: string = await response.text();
+  return;
+}
