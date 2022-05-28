86fae6381b08624c30bca5fb35a1c49d96c961af
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-05-28 18:53:18 +0700

    implemented handle-rejection cases

--- src/core/process/handle-rejection.ts
+++ src/core/process/handle-rejection.ts
@@ -1,5 +1,21 @@
-const WHITELIST_ERRORS: string[] = ["Input stream error: Video unavailable"];
+const WHITELIST_ERRORS: string[] = ["Input stream error", "No video id found"];
+
+const WHITELIST_EXCEPTIONS: string[] = ["Input stream error"];
 
 process.on("unhandledRejection", (err: any) => {
-  if (!WHITELIST_ERRORS.includes(err.message)) process.exit(1);
+  if (
+    !WHITELIST_ERRORS.some((whitelistError) =>
+      err.message.includes(whitelistError)
+    )
+  )
+    process.exit(1);
+});
+
+process.on("uncaughtException", function (err) {
+  if (
+    !WHITELIST_EXCEPTIONS.some((whitelistError) =>
+      err.message.includes(whitelistError)
+    )
+  )
+    process.exit(1);
 });
