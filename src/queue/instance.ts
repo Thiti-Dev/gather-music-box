d018fe6f0458ee70512af190f242536524d2c5d5
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-06-05 22:26:29 +0700

    queue.instance -> implemented createInstanceSenderChannel & sendMessage

--- src/queue/instance.ts
+++ src/queue/instance.ts
@@ -2,11 +2,23 @@ import amqplib from "amqplib";
 
 export class QueueInstance {
   private static instance: amqplib.Connection;
+  private static senderChannel: amqplib.Channel;
   public static setInstance(instance: amqplib.Connection) {
     this.instance = instance;
     console.log("[QueueInstance](class): Queue instance has benn initiated");
+    this.createInstanceSenderChannel();
+    require("../core/queue-consumer"); // set up the consumer
   }
   public static getInstance(): amqplib.Connection {
     return this.instance;
+  }
+  public static async createInstanceSenderChannel() {
+    const senderCH: amqplib.Channel = await this.instance.createChannel();
+    this.senderChannel = senderCH;
+    console.log("[QueueInstance](class): Sender Channel has been initiated");
+  }
+
+  public static sendMessage(queueName: string, message: string) {
+    this.senderChannel.sendToQueue(queueName, Buffer.from(message));
   }
 }
