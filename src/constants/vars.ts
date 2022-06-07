f6cfe3e00e46e4b2e38bfce13e87cb15dc6450a4
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-06-07 23:42:43 +0700

    added related constant vars

--- src/constants/vars.ts
+++ src/constants/vars.ts
@@ -2,3 +2,5 @@ export const GATHER_GET_MAP_URL: string = "https://gather.town/api/getMap";
 export const GATHER_SET_MAP_URL: string = "https://gather.town/api/setMap";
 
 export const PREDEFINED_GATHER_SOUND_EMITTER_OBJ_STRINGIFIED: string = `{"sound":{"volume":0.1,"loop":false,"src":"https://firebasestorage.googleapis.com/v0/b/gather-town.appspot.com/o/manually-uploaded%2FNewTimGillAllStars-001.mp3?alt=media&token=ccd6358f-2fea-4bde-8780-40fe850bc877","maxDistance":8},"type":0,"properties":{},"id":"RIPTimGill - M28uALsX665f41YU-GOxI_7ad7f7c8-29c8-4187-8500-eb8d826e546e","x":13,"y":57,"highlighted":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/internal-dashboard/images/OwBsRbmjelaJNKQy7c0vB","_name":"Gravestone of tim gill","objectPlacerId":"rYvWNmOZo3WO59Y6Y84x84b8wul1","templateId":"RIPTimGill - M28uALsX665f41YU-GOxI","orientation":0,"offsetX":0,"_tags":["sounds","funeral","memorial"],"offsetY":0.5,"height":2,"width":1,"color":"a","normal":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/internal-dashboard/images/On5R_QLMEfZ0ef_v6OilV"}`;
+
+export const M_QUEUE_QUEUE_NAME: string = "music-queue";
